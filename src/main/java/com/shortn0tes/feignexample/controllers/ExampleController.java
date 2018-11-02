8a1646b12f5be48a76797c6eb5b6347797011e7c
Anna Shipilova <anyashi2002@gmail.com>
2018-11-02 15:54:16 +0200

    Some lines were deleted

--- src/main/java/com/shortn0tes/feignexample/controllers/ExampleController.java
+++ src/main/java/com/shortn0tes/feignexample/controllers/ExampleController.java
@@ -1,31 +1,16 @@
 package com.shortn0tes.feignexample.controllers;
 
-import com.shortn0tes.feignexample.feign.BitfenixObjectClient;
 import com.shortn0tes.feignexample.feign.ExmoObjectClient;
 import com.shortn0tes.feignexample.feign.HitBtcTradeClient;
-import com.shortn0tes.feignexample.feign.HitbtcObjectClient;
-import com.shortn0tes.feignexample.model.Price;
-import com.shortn0tes.feignexample.model.Profit;
 import com.shortn0tes.feignexample.model.Trade;
-import com.shortn0tes.feignexample.model.bitfenix.AsksBF;
-import com.shortn0tes.feignexample.model.bitfenix.BidsBF;
-import com.shortn0tes.feignexample.model.bitfenix.BitfenixObject;
 import com.shortn0tes.feignexample.model.exmo.ExmoObject;
-import com.shortn0tes.feignexample.model.hitbtc.AskH;
-import com.shortn0tes.feignexample.model.hitbtc.BidH;
-import com.shortn0tes.feignexample.model.hitbtc.HitbtcObject;
-import com.shortn0tes.feignexample.repos.PriceRepo;
-import com.shortn0tes.feignexample.repos.ProfitRepo;
 import com.shortn0tes.feignexample.repos.TradeRepo;
 import com.shortn0tes.feignexample.trades.HitBtcTrade;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
 
-import java.awt.*;
 import java.util.Date;
-import java.util.HashSet;
-import java.util.Iterator;
 import java.util.Set;
 
 
@@ -44,7 +29,7 @@ public class ExampleController {
     TradeRepo tradeRepo;
 
 
-    @Scheduled(fixedRate = 10000)
+    @Scheduled(fixedRate = 60000)
     void add() {
 
         if (hitBtcTradeSet == null) {
@@ -62,7 +47,6 @@ public class ExampleController {
 
                 //we need this quantity
                 double quantity = Double.valueOf(temp.getQuantity());
-                System.out.println("we need " + quantity);
                 double priceFirst = Double.valueOf(temp.getPrice()) * quantity;
 
                 String[][] ask = exmoObject.getBook().getAsk();
@@ -76,12 +60,10 @@ public class ExampleController {
                     if (quantityExmo >= quantity) {
                         if (thisQuantity == 0) {
                             priceSecond = price * quantity;
-                            System.out.println("priceSecond: " + priceSecond);
                             break;
                         } else {
                             double result = quantity - thisQuantity;
                             priceSecond = result * price + counted;
-                            System.out.println("priceSecond: " + priceSecond);
                             break;
                         }
                     } else {
